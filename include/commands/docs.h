9d5f59eb9492844cb817d1665b92612748d624eb
drpidman <augustocaio663@gmail.com>
2023-06-28 23:56:20 -0300

    Add interactive language selection to the 'docs' command
    This commit extends the functionality of the 'docs' command, allowing users to select the language they want detailed information about. The new feature engages users more, tailoring responses according to their chosen programming language. It also offers individual language details such as NodeJS, Python, C++, and C# via a variant type. Switch cases handle each language, customizing the information shown based on the chosen language. Apart from this enhancement, files in the .idea directory are also added to the repository, which configures the CMake system and module structure of the project for better syncing across different development environments. Lastly, .gitignore is updated to exclude certain .idea subdirectories that may contain user-specific data.

--- include/commands/docs.h
+++ include/commands/docs.h
@@ -5,7 +5,16 @@
 #include "CommandNames.h"
 
 
-extern void command_docs_run(const dpp::slashcommand_t& event, const dpp::cluster& bot);
+enum Langs {
+    NodeJS,
+    Python,
+    Cpp,
+    Csharp
+};
+
+Langs resolveLang(const std::string& lang);
+
+void command_docs_run(const dpp::slashcommand_t& event, const dpp::cluster& bot);
 
 namespace CommandNames {
     void RegisterCInfosCommand(dpp::cluster& bot);
